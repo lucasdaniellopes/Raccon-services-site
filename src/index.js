8ce4426cc8f06e92b4cd8f81603c6b7106829401
lucasdaniellopes <lucasdanielspead@gmail.com>
2024-06-25 18:04:11 -0300

    add faq section

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ function onToggleMenu(e) {
   navbar.classList.toggle("opacity-100");
 }
 
-function changeArrow(e) {
+function changeHeaderArrow(e) {
   const navbar = e.childNodes[1].childNodes[3];
   navbar.name = navbar.name === "caret-down" ? "caret-up" : "caret-down";
+}
+function toggleAnswer(id) {
+  const answer = document.getElementById(`answer-${id}`);
+  answer.classList.toggle("hidden");
+  answer.parentElement.classList.toggle("scale-[1.05]");
+}
+
+function changeFaqArrow(e) {
+  const accordion = e.childNodes[1].childNodes[3].childNodes[1];
+  accordion.name =
+    accordion.name === "chevron-down-outline"
+      ? "chevron-up-outline"
+      : "chevron-down-outline";
 }
