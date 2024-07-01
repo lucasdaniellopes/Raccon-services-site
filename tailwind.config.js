158671a89292f9040f34ae990e1e31502dfb6546
lucasdaniellopes <lucasdanielspead@gmail.com>
2024-07-01 17:28:36 -0300

    add: nfa page finalized

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,18 @@ module.exports = {
         bronzeShadowColor: "#441C27",
         goldShadowColor: "#86582F",
         platinumShadowColor: "#2B7692",
+        emeraldShadowColor: "#054830",
+        diamondShadowColor: "#140A3E",
+        masterShadowColor: "#9142CA",
+        grandmasterShadowColor: "#7C2E35",
+        challengerShadowColor: "#193E6B",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
       boxShadow: {
         glow: ["0 0px 150px 10px"],
+        bgGlow: ["0 0px 0px 10px"],
       },
     },
   },
